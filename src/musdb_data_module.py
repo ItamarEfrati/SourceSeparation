c0efb3f34ce83136201b9a7c384bd71fc98f61f6
ItamarEfrati <itamar.efr@gmail.com>
2023-02-17 13:46:15 +0200

    initial commit

---
+++ src/musdb_data_module.py
@@ -0,0 +1,66 @@
+import os
+import pickle
+
+import pytorch_lightning as pl
+
+from torch.utils.data import DataLoader
+from src.spectorgram_dataset import SpectrogramDataset, basic_collate
+
+
+class MUSDBDataModule(pl.LightningDataModule):
+    def __init__(self,
+                 data_dir: str = "../data/musdb18",
+                 stft_frames=25,
+                 stft_stride=1,
+                 hop_size=256,
+                 fft_size=1024,
+                 mel_freqs=None,
+                 fmin=20,
+                 min_level_db=-100,
+                 ref_level_db=20,
+                 batch_size: int = 32,
+                 train_mask_threshold=0.5,
+                 test_mask_threshold=0.1):
+        super().__init__()
+        self.offset = stft_frames // 2
+        self.save_hyperparameters()
+
+    def _split_wav(self, wav_mix, wav_vocal, track_name):
+        size = wav_mix.shape[0] - self.hparams.stft_frames
+        for i in range(size, self.hparams.stft_stride):
+            j = i + self.hparams.stft_frames
+            x = wav_mix[:, :, i:j]
+            y = wav_vocal[:, i + self.offset]
+
+    def prepare_data(self) -> None:
+        pass
+
+    def setup(self, stage: str = None):
+        with open(os.path.join(self.hparams.data_dir, 'spec_info.pkl'), 'rb') as f:
+            train_specs = pickle.load(f)
+        test_path = os.path.join(self.hparams.data_dir, "test")
+        with open(os.path.join(test_path, "test_spec_info.pkl"), 'rb') as f:
+            test_specs = pickle.load(f)
+
+        test_path = os.path.join(self.hparams.data_dir, "test")
+        self.train_data = SpectrogramDataset(self.hparams.data_dir, train_specs, self.hparams.stft_frames,
+                                             self.hparams.stft_stride)
+        self.test_data = SpectrogramDataset(test_path, test_specs, self.hparams.stft_frames, self.hparams.stft_stride)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_data, batch_size=self.hparams.batch_size,
+                          collate_fn=lambda b: basic_collate(b, self.hparams.train_mask_threshold))
+
+    # def val_dataloader(self):
+    #     return DataLoader(self.val_data, batch_size=self.hparams.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_data, batch_size=self.hparams.batch_size,
+                          collate_fn=lambda b: basic_collate(b, self.hparams.test_mask_threshold))
+
+
+if __name__ == '__main__':
+    d = MUSDBDataModule()
+    d.setup()
+    t_d = d.train_dataloader()
+    print(1)
---
+++ src/spectorgram_dataset.py
@@ -0,0 +1,52 @@
+import os
+import numpy as np
+import torch
+
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+
+class SpectrogramDataset(Dataset):
+    def __init__(self, data_path, spec_info, stft_frames, stft_stride):
+        self.data_path = data_path
+        self.mix_path = os.path.join(data_path, "mix")
+        self.vox_path = os.path.join(data_path, "vox")
+        self.stft_frames = stft_frames
+        self.stft_stride = stft_stride
+        self.offset = stft_frames // 2
+        self.metadata = self.get_slices(spec_info)
+
+    def get_slices(self, spec_info):
+        metadata = []
+        print("Preparing dataset")
+        for spec in tqdm(spec_info):
+            size = spec[1] - self.stft_frames
+            for i in range(0, size, self.stft_stride):
+                j = i + self.stft_frames
+                slice_info = (spec[0], i, j)
+                metadata.append(slice_info)
+        return metadata
+
+    def __getitem__(self, index):
+        slice_info = self.metadata[index]
+        fname = slice_info[0] + ".npy"
+        i = slice_info[1]
+        j = slice_info[2]
+        x = np.load(os.path.join(self.mix_path, fname), mmap_mode='r')[:, :, i:j]
+        y = np.load(os.path.join(self.vox_path, fname), mmap_mode='r')[:, i + self.offset]
+        return x, y
+
+    def __len__(self):
+        return len(self.metadata)
+
+
+def basic_collate(batch, mask_threshold):
+    x = [it[0] for it in batch]
+    x = np.stack(x).astype(np.float32)
+    x = torch.FloatTensor(x)
+    y = [it[1] for it in batch]
+    y = np.stack(y)
+    if mask_threshold is not None:
+        y = y > mask_threshold
+    y = torch.FloatTensor(y.astype(np.float32))
+    return x, y
